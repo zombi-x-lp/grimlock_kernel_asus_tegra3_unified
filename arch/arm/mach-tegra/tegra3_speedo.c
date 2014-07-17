dbd6f39634a6e42cb9dd1c34091b9cb7185638bf
Hardslog <hardslog@gmail.com>
2014-07-17 10:28:39 -0400

    remove fsync dsabler as there is already dymanic fsync

--- arch/arm/mach-tegra/tegra3_speedo.c
+++ arch/arm/mach-tegra/tegra3_speedo.c
@@ -554,7 +554,7 @@ int tegra_package_id(void)
  */
 static const int cpu_speedo_nominal_millivolts[] =
 /* speedo_id 0,    1,    2,    3,    4,    5,    6,    7,    8,   9,  10,  11,   12,    13,  14,  15 */
-	{ 1125, 1150, 1150, 1150, 1237, 1237, 1237, 1150, 1150, 1007, 916, 850, 1237, 1237, 950, 900};
+	{ 1125, 1150, 1150, 1150, 1237, 1237, 1237, 1237, 1150, 1007, 916, 850, 1237, 1237, 950, 900};
 
 int tegra_cpu_speedo_mv(void)
 {
@@ -569,7 +569,7 @@ int tegra_core_speedo_mv(void)
 		return 1200;
 	case 1:
 		if ((cpu_speedo_id != 7) && (cpu_speedo_id != 8))
-			return 1200;
+			return 1250;
 		/* fall thru for T30L or T30SL */
 	case 2:
 		if (cpu_speedo_id != 13)
